a552457e6204841f013738bf1eaebee3275a3f0f
Aniket-Jadhav27 <aniketsjadhav567@gmail.com>
2025-07-26 19:10:25 +0530

    Initial commit

---
+++ i.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int i=0;
+	int j=0;
+	printf("Enter any two number=");
+	
+	scanf("%d%d",&i,&j);
+	
+	printf ("addition of two numbers=%d",i+j);
+	return 0;
+	
+}
---
+++ s.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+int a=0;
+printf("enter any number=s");
+scanf("%d",&a);
+return 0;
+}
