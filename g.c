0115a5714147fbab82bf4fe83ed79a758d80242f
Aniket-Jadhav27 <aniketsjadhav567@gmail.com>
2025-08-08 22:34:16 +0530

    int are equal

---
+++ g.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+	int a, b;
+	
+	printf("enter 1st numbers:");
+	scanf("%d",&a);
+
+    printf("enter 2nd numbers:");
+	scanf("%d",&b);
+	
+	if(a==b)
+     
+		printf("A and B are Equal");
+	
+	else if(a>b)
+    
+		printf(" a is gerater than b :");
+   
+	
+	else
+        
+		printf("b is greater than a :");
+		
+	
+	
+	return 0;
+	}
