8e5366b61b2c9f2bcbc582136d6975c7188241c0
Aniket-Jadhav27 <aniketsjadhav567@gmail.com>
2026-09-10 19:30:29 +0530

    Add files via upload

---
+++ character.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    char ch='C';
+    printf("Original charcter is :%c\n",ch);
+    printf("Previous character is:%c\n",ch-1);
+    printf("Next character is:%c",ch+1);
+    return 0;
+}
